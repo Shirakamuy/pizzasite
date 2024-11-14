1447d802b909cabbc7facf40be3a5d42f9d5100b
Erik <erikrich2003@gmail.com>
2024-11-14 16:59:58 +0100

    dz

--- app.py
+++ app.py
@@ -3,12 +3,6 @@ import sqlite3
 
 app = Flask(__name__)
 
-pizzas = [
-    {"name": "Одне Сало", "price": 170, "ingredients": "Смалець, сало свиняче копчене, бекон", "image_url": "https://tashirpizza.ru/images/products/44-1374.png"},
-    {"name": "Тисяча градусів за цельсієм", "price": 220, "ingredients": "Соус Табаско, чорізо, сир пекорино, перець халапеньйо", "image_url": "https://i0.wp.com/www.izumi-sushi.kz/wp-content/uploads/D181D0B0D0BBD18FD0BCD0B8.webp?fit=300%2C300&ssl=1"},
-    {"name": "Чотири соуси", "price": 135, "ingredients": "Соус томатний, соус мексиканьский, соус американьский, чосниковий соус", "image_url": "https://sergiopizza.ru/upload/iblock/4f4/4f4b7fa79e8465500baa585cf4f8b803.png"}
-]
-
 @app.get("/")
 def first_page():
     return render_template("firstpage.html", title="Три ківбаски", order="Замовлення", news="Новини", menu_pizza="Меню піццерії")
@@ -38,15 +32,15 @@ try:
     cursor.execute(create_table_query)
 
     insert_query = '''INSERT INTO pizzas_list (id, name, price, ingredients, image)
-    VALUES (1, "Одне сало", "170", "Смалець, сало свиняче копчене, бекон", "https://tashirpizza.ru/images/products/44-1374.png")'''
+    VALUES (1, "Одне сало", "170", "Смалець, сало свиняче копчене, бекон", "https://files.oaiusercontent.com/file-4LZ9LEt60GY3ADcqeGgnskHU?se=2024-11-14T15%3A55%3A44Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D1e0a7a34-165a-4fc8-8e6a-ac1e712ee0d4.webp&sig=sP%2B1KNfTT2OmtxNWvzCx39QsQshIr/MgJgMEG7X7gtc%3D")'''
     cursor.execute(insert_query)
 
     insert_query = '''INSERT INTO  pizzas_list (id, name, price, ingredients, image)
-    VALUES (2, "Тисяча градусів за цельсієм", "220", "Соус Табаско, чорізо, сир пекорино, перець халапеньйо", "https://i0.wp.com/www.izumi-sushi.kz/wp-content/uploads/D181D0B0D0BBD18FD0BCD0B8.webp?fit=300%2C300&ssl=1")'''
+    VALUES (2, "Тисяча градусів за цельсієм", "220", "Соус Табаско, чорізо, сир пекорино, перець халапеньйо", "https://files.oaiusercontent.com/file-yridI9zuglCgwKKlpj3Ax1AT?se=2024-11-14T15%3A54%3A36Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D17bc54c8-cb38-49cd-adf1-2f42a2ab596a.webp&sig=uD09QepqIx/6BXPX9jp47voK5JXK4h2dshO0ijCmL90%3D")'''
     cursor.execute(insert_query)
 
     insert_query = '''INSERT INTO  pizzas_list (id, name, price, ingredients, image)
-        VALUES (3, "Чотири соуси", "135", "Соус томатний, соус мексиканьский, соус американьский, чосниковий соус", "https://sergiopizza.ru/upload/iblock/4f4/4f4b7fa79e8465500baa585cf4f8b803.png")'''
+        VALUES (3, "Чотири соуси", "135", "Соус томатний, соус мексиканьский, соус американьский, чосниковий соус", "https://files.oaiusercontent.com/file-JDYl26QAJCRWU8bdw13GqnCR?se=2024-11-14T15%3A56%3A47Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D37cbb33e-944e-427f-98f4-8fbab810ba6f.webp&sig=sU4sSE%2ByCpOdrPp1hmlddS2tVdRNU8VbWKOykyTD8os%3D")'''
     cursor.execute(insert_query)
 
     sqlite_connection.commit()
@@ -65,6 +59,15 @@ finally:
     if (sqlite_connection):
         sqlite_connection.close()
         print("Connection with SQL closed")
+
+def get_all_pizzas():
+    sqlite_connection = sqlite3.connect("sql_python.db")
+    cursor = sqlite_connection.cursor()
+    cursor.execute("SELECT * FROM pizzas_list")
+    pizzas = cursor.fetchall()
+    return [{"id": row[0], "name": row[1], "price": row[2], "ingredients": row[3], "image": row[4]} for row in pizzas]
+
+pizzas = get_all_pizzas()
 
 
 def save_to_db(id, name, email, joing_date, salary):
